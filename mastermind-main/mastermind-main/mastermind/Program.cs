288db067df1dc99de6c0f88b0239ac8698c2050d
EwanR5 <91669749+EwanR5@users.noreply.github.com>
2021-10-21 14:27:32 +0200

    commencement du mastermind

---
+++ mastermind-main/mastermind-main/mastermind/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string combinaison;
+            int[] proposition;
+            int[] combi;
+            ChoixCombi(out combi);
+            Afficher(combi);
+            Console.WriteLine("Définis les nombres choisis");
+            combinaison = Console.ReadLine();
+            StringToArray(combinaison, out proposition);
+            Afficher(proposition);
+        }
+
+
+        static void ChoixCombi(out int[] combi)
+        {
+            combi = new int[4];
+            Random rnd = new Random();
+            for (int i = 0; i <= 3; i++)
+            {
+                int valeur = rnd.Next(1, 7);
+                combi[i] = valeur;
+            }
+        }
+
+        static void Afficher(int[] combi)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.Write(combi[i]);
+            }
+        }
+
+        static void StringToArray(string combinaison, out int[] proposition)
+        {
+            proposition = new int[4];
+            for (int i = 0; i <= 3; i++)
+            {
+                proposition[i] = combinaison[i];
+            }
+        }
+    }
+}
